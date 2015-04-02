c3d75e19ff9688cafa384c9013dad9be559ebe8d
Naser Afsar <nafsar@gmail.com>
2015-04-01 21:51:33 -0600

    new files
    adding contents

---
+++ js/app.js
@@ -0,0 +1,22 @@
+"use strict";
+(function () {
+    var app = angular.module('myStore', ['store-directives']);
+
+    app.controller('StoreController', ['$http', function ($http) {
+        var store = this;
+        store.products = [];
+        $http.get('js/store-products.json').success(function (data) {
+            store.products = data;
+        });
+    }]);
+
+    app.controller('ReviewController', function () {
+        this.review = {};
+
+        this.addReview = function (product) {
+            product.reviews.push(this.review);
+
+            this.review = {};
+        };
+    });
+})();
